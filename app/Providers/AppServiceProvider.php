e67309000aeb81d5935b2cc21bdd4d399da8c26a
Darta <80467516+gdarta@users.noreply.github.com>
2023-06-20 17:12:28 +0300

    Added vendor files for pagination formatting.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
@@ -23,5 +24,6 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         Model::unguard();
+        //Paginator::useBootstrapFour();
     }
 }
