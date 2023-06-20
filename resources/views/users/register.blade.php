2ee6dd7df123fda2f7d7480e8e6bbf13ca49d19e
R4nd0mF0x <118675869+R4nd0mF0x@users.noreply.github.com>
2023-06-20 16:53:09 +0300

    Register css and password field type change

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -17,37 +17,37 @@
     <div id="post">
         <form method='POST' action='./users'>
             @csrf
-            <label for='name'>Enter your name: </label>
+            <label for='name'><h2>Enter your name: </h2></label>
             <input type='text' name='name' id='name' value="{{ old('name') }}"> <br>
             @error('name')
             <p class="error">{{ $message }}</p>
             @enderror
 
-            <label for='username'>Enter your username: </label>
+            <label for='username'><h2>Enter your username: </h2></label>
             <input type='text' name='username' id='username' value="{{ old('username') }}"> <br>
             @error('username')
             <p class="error">{{ $message }}</p>
             @enderror
 
-            <label for='email'>Enter your e-mail: </label>
+            <label for='email'><h2>Enter your e-mail: </h2></label>
             <input type='text' name='email' id='email' value="{{ old('email') }}"> <br>
             @error('email')
             <p class="error">{{ $message }}</p>
             @enderror
 
-            <label for='password'>Enter password: </label>
-            <input type='text' name='password' id='password' value="{{ old('password') }}"> <br>
+            <label for='password'><h2>Enter password: </h2></label>
+            <input type='password' name='password' id='password' value="{{ old('password') }}"> <br>
             @error('password')
             <p class="error">{{ $message }}</p>
             @enderror
 
-            <label for='password_confirmation'>Confirm password: </label>
-            <input type='text' name='password_confirmation' id='password_confirmation'> <br>
+            <label for='password_confirmation'><h2>Confirm password: </h2></label>
+            <input type='password' name='password_confirmation' id='password_confirmation'> <br>
             @error('password_confirmation')
             <p class="error">{{ $message }}</p>
             @enderror
 
-            <button type='submit'>Register</button>
+            <br><button type='submit' class="button">Register</button>
         </form>
     </div>
     <p>Already registered? <a href='./login'>Login</a></p>
