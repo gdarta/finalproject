b58bf29195eee54c88e9a095bcac0e20261e7bfd
R4nd0mF0x <random.fox007@gmail.com>
2023-06-21 03:50:13 +0300

    Big one

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -13,27 +13,27 @@
             <p>HANG!</p>
         </div>
     </a>
-    <h1>Log into your account to post sweaters</h1>
+    <h1>{{ __('msg.log_into_your_account_to_post_your_sweaters')}}</h1>
     <div id="post">
         <form method='POST' action='./users/authenticate'>
             @csrf
             <!--TODO: add username authentication-->
-            <label for='email'><h2>Enter your e-mail: </h2></label>
+            <label for='email'><h2>{{ __('msg.enter_your_e-mail')}}: </h2></label>
             <input type='text' name='email' id='email' value="{{ old('email') }}"> <br>
             @error('email')
             <p class="error">{{ $message }}</p>
             @enderror
 
-            <label for='password'><h2>Enter password: </h2></label>
+            <label for='password'><h2>{{ __('msg.enter_password')}}: </h2></label>
             <input type='text' name='password' id='password'> <br>
             @error('password')
             <p class="error">{{ $message }}</p>
             @enderror
 
-            <br><button type='submit' class="button">Login</button>
+            <br><button type='submit' class="button">{{ __('msg.login')}}</button>
         </form>
     </div>
-    <p>Are not registered? <a href='./register'>Register</a></p>
+    <p>{{ __('msg.are_not_registered')}} <a href='./register'>{{ __('msg.register')}}</a></p>
 </body>
 
 </html>
